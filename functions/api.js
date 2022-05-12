74444b328cf362ba8a309a697f88f9fa4e77b624
Jonas D <jonas.duri@gmail.com>
2022-05-12 16:14:39 +0200

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,8 @@
 exports.handler = async function (event, context) {
+  const d = Date.now()
+  const currentDate = new Date(d)
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "Hello World", date: currentDate.toUTCString() }),
   };
 }
